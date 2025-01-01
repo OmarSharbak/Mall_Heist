aab95d7c1852bcb6b135b11219f6852f06f8f4ae
lukasmakegames <lugiro43@gmail.com>
2025-01-01 16:24:06 +0100

    fix singleplayer load levels

--- Assets/Scripts/Network/CheckSinglePlayer.cs
+++ Assets/Scripts/Network/CheckSinglePlayer.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.SceneManagement;
 public class CheckSinglePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 		MultiplayerMode multiplayerMode = FindObjectOfType<MultiplayerMode>();
-		if ((multiplayerMode!=null && multiplayerMode.isSinglePlayer) || multiplayerMode==null)
+		if ((multiplayerMode != null && multiplayerMode.isSinglePlayer) || multiplayerMode == null)
 		{
+			string name = "Level" + multiplayerMode.lvlIndex;
+			Debug.Log(name);
+			CustomNetworkManager customNetworkManager = GetComponent<CustomNetworkManager>();
+			customNetworkManager.onlineScene = name;
 			NetworkServer.dontListen = true;
 			NetworkManager.singleton.StartHost();
 		}
