1e6125b92e46fc4114d465d172664eca9ed00055
lukasmakegames <lugiro43@gmail.com>
2025-01-06 19:42:11 +0100

    add lobby members

--- Assets/Scripts/Network/Lobby/LobbyMenuManager.cs
+++ Assets/Scripts/Network/Lobby/LobbyMenuManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class LobbyMenuManager : MonoBehaviour
 {
@@ -14,6 +15,13 @@ public class LobbyMenuManager : MonoBehaviour
 	[SerializeField] private TextMeshProUGUI lobbyTitle;
 	[SerializeField] private LobbyManager lobbyManager;
 
+
+	[Header("User lobby setup")]
+	[SerializeField] private LobbyUserPanel lobbyUserPanel;
+	[SerializeField] private Transform lobbyUserHolder;
+
+
+	private Dictionary<UserData, LobbyUserPanel> _lobbyUserPanels = new();
 	private void Awake()
 	{
 		OpenMainMenu();
@@ -22,23 +30,28 @@ public class LobbyMenuManager : MonoBehaviour
 
 	public void OnLobbyCreated(LobbyData lobbyData)
 	{
+		ClearCards();
 		lobbyData.Name= UserData.Me.Name+ "'s Lobby";
 		lobbyTitle.text= UserData.Me.Name+ "'s Lobby";
 		OpenLobby();
+
+		SetupCard(UserData.Me);
 
 	}
 
 	public void OnLobbyJoined(LobbyData lobbyData)
 	{
-		lobbyData.Name = lobbyData.Name;
+		ClearCards();
 		lobbyTitle.text = lobbyData.Name;
 		OpenLobby();
 
+		foreach(var member in lobbyData.Members)
+			SetupCard(member.user);
 	}
 
 	private void OverlayJoinButton(LobbyData lobbyData, UserData userData)
 	{
-		throw new NotImplementedException();
+		lobbyManager.Join(lobbyData);
 	}
 
 	public void OpenMainMenu()
@@ -53,10 +66,37 @@ public class LobbyMenuManager : MonoBehaviour
 		lobbyObject.SetActive(true);
 	}
 
+	public void OnUserJoin(UserData userData)
+	{
+		SetupCard(userData);
+	}
+	public void OnUserleft(UserLobbyLeaveData userLeaveData)
+	{
+		if(!_lobbyUserPanels.TryGetValue(userLeaveData.user, out var panel))
+		{
+		}
+		Destroy(panel.gameObject);
+		_lobbyUserPanels.Remove(userLeaveData.user);
+	}
+
 	private void CloseScreens()
 	{
 		mainMenuObject.SetActive(false);
 		lobbyObject.SetActive(false);
+	}
+
+	private void ClearCards()
+	{
+		foreach(Transform child in lobbyUserHolder)
+			Destroy(child.gameObject);
+
+		_lobbyUserPanels.Clear();
+	}
+	private void SetupCard(UserData userData)
+	{
+		var userPanel = Instantiate(lobbyUserPanel, lobbyUserHolder);
+		userPanel.Initialize(userData);
+		_lobbyUserPanels.TryAdd(userData,userPanel);
 	}
 
 }
